37c05aae1d8435b906a14bd15b98fe2983af17c7
Erdinch <erdinchorhan02@gmail.com>
2022-09-08 20:50:47 +0300

    testing stuff /w jest

---
+++ tests/ship.test.js
@@ -0,0 +1,14 @@
+import Ship from '../src/ship';
+
+// const carrier = new Ship('Carrier', 5);
+
+// test
+
+// test('returns default "Carrier" ship object', () => {
+//   expect(new Ship('Carrier', 5)).toEqual({
+//     shipClass: 'Carrier',
+//     length: 5,
+//     hit: false,
+//     sunk: false,
+//   });
+// });
