7e1636d104761bbfff9bbdb10296bf7261058bd4
Erdinch <erdinchorhan02@gmail.com>
2022-09-10 00:23:02 +0300

    simple gameboard test

---
+++ tests/gameboard.test.js
@@ -0,0 +1,25 @@
+import Gameboard from '../src/gameboard';
+import Ship from '../src/ship';
+
+let gameboard = Gameboard;
+let ship = Ship;
+
+beforeEach(() => {
+  gameboard = new Gameboard();
+  ship = new Ship('Destroyer');
+});
+
+test('creates gameboard', () => {
+  expect(gameboard.gameboard).toEqual([
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+  ]);
+});
