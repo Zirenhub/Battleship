cdf156be521062af44526f90442d609d6197b88b
Erdinch <erdinchorhan02@gmail.com>
2022-09-10 13:31:33 +0300

    some simple tests

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,25 +1,87 @@
 import Gameboard from '../src/gameboard';
-import Ship from '../src/ship';
 
-let gameboard = Gameboard;
-let ship = Ship;
+describe('create gameboard', () => {
+  const gameboard = new Gameboard();
 
-beforeEach(() => {
-  gameboard = new Gameboard();
-  ship = new Ship('Destroyer');
+  test('creates gameboard', () => {
+    expect(gameboard.gameboard).toEqual([
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ]);
+  });
 });
 
-test('creates gameboard', () => {
-  expect(gameboard.gameboard).toEqual([
-    [null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null],
-  ]);
+let gameboard;
+
+describe('placing ships', () => {
+  beforeEach(() => {
+    gameboard = new Gameboard();
+  });
+
+  test('place "Destroyer" at [0,0] in vertical pos', () => {
+    gameboard.placeShip('Destroyer', [0, 0], 'ver');
+    expect(gameboard.gameboard).toEqual([
+      ['Destroyer', null, null, null, null, null, null, null, null, null],
+      ['Destroyer', null, null, null, null, null, null, null, null, null],
+      ['Destroyer', null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ]);
+  });
+
+  test('place "Submarine" at [5, 5] in horizontal pos', () => {
+    gameboard.placeShip('Submarine', [5, 5], 'hor');
+    expect(gameboard.gameboard).toEqual([
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null], // formatting issue
+      [
+        null,
+        null,
+        null,
+        null,
+        null,
+        'Submarine',
+        'Submarine',
+        'Submarine',
+        null,
+        null,
+      ],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ]);
+  });
+
+  test('place "Carrier" at [2, 2] in vertical pos', () => {
+    gameboard.placeShip('Carrier', [2, 2], 'ver');
+    expect(gameboard.gameboard).toEqual([
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, 'Carrier', null, null, null, null, null, null, null],
+      [null, null, 'Carrier', null, null, null, null, null, null, null],
+      [null, null, 'Carrier', null, null, null, null, null, null, null],
+      [null, null, 'Carrier', null, null, null, null, null, null, null],
+      [null, null, 'Carrier', null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ]);
+  });
 });
