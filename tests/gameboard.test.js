142c0fc7fdc8ff69b3d0bd3d6ebd95a4731aff4c
Erdinch <erdinchorhan02@gmail.com>
2022-09-11 19:27:26 +0300

    ship hitting and check if all sunk tests

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -198,3 +198,110 @@ describe('placing ships at invalid pos', () => {
     ]);
   });
 });
+
+describe('hitting ships', () => {
+  beforeEach(() => {
+    gameboard = new Gameboard();
+    logSpy = jest.spyOn(console, 'log');
+  });
+
+  test('hit "Carrier" from position 3', () => {
+    gameboard.placeShip('Carrier', [5, 2], 'hor');
+    gameboard.receiveAttack([5, 4]);
+    expect(logSpy).toHaveBeenCalledWith('hitting Carrier from position 3');
+    // check if ship's length/position array also registered the hit.
+    expect(gameboard.ships[0].length[2]).toEqual('hit');
+    expect(gameboard.gameboard).toEqual([
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, 'Car', 'Car', 'hit', 'Car', 'Car', null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ]);
+  });
+
+  test('hit "Submarine" from position 1', () => {
+    gameboard.placeShip('Submarine', [3, 3], 'ver');
+    gameboard.placeShip('Destroyer', [4, 2], 'hor');
+    gameboard.receiveAttack([3, 3]);
+    expect(logSpy).toHaveBeenCalledWith('Sub 3 / 3 FILLED');
+    expect(logSpy).toHaveBeenCalledWith(
+      'occupied by ship nearby (sides or top/bottom and edges)'
+    );
+    expect(logSpy).toHaveBeenCalledWith('hitting Submarine from position 1');
+    expect(gameboard.ships[0].length[0]).toEqual('hit');
+    expect(gameboard.gameboard).toEqual([
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, 'hit', null, null, null, null, null, null],
+      [null, null, null, 'Sub', null, null, null, null, null, null],
+      [null, null, null, 'Sub', null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ]);
+  });
+
+  test('sink a "Battleship"', () => {
+    gameboard.placeShip('Battleship', [5, 5], 'ver');
+    gameboard.receiveAttack([5, 5]);
+    gameboard.receiveAttack([6, 5]);
+    gameboard.receiveAttack([7, 5]);
+    gameboard.receiveAttack([8, 5]);
+    expect(gameboard.ships[0].length[0]).toEqual('hit');
+    expect(gameboard.ships[0].length[1]).toEqual('hit');
+    expect(gameboard.ships[0].length[2]).toEqual('hit');
+    expect(gameboard.ships[0].length[3]).toEqual('hit');
+    expect(gameboard.ships[0].isSunk).toBeTruthy();
+    expect(gameboard.gameboard).toEqual([
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, 'hit', null, null, null, null],
+      [null, null, null, null, null, 'hit', null, null, null, null],
+      [null, null, null, null, null, 'hit', null, null, null, null],
+      [null, null, null, null, null, 'hit', null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ]);
+  });
+
+  test('all ships to be sunk', () => {
+    gameboard.placeShip('Carrier', [3, 3], 'ver');
+    gameboard.placeShip('Battleship', [0, 5], 'hor');
+    gameboard.placeShip('Destroyer', [6, 6], 'ver');
+    gameboard.placeShip('Submarine', [5, 1], 'ver');
+    gameboard.placeShip('Patrol Boat', [7, 8], 'ver');
+    gameboard.receiveAttack([3, 3]); //-------
+    gameboard.receiveAttack([4, 3]); //      |
+    gameboard.receiveAttack([5, 3]); //      | Carrier
+    gameboard.receiveAttack([6, 3]); //      |
+    gameboard.receiveAttack([7, 3]); //-------
+
+    gameboard.receiveAttack([0, 5]); //-------
+    gameboard.receiveAttack([0, 6]); //      | Battleship
+    gameboard.receiveAttack([0, 7]); //      |
+    gameboard.receiveAttack([0, 8]); //-------
+
+    gameboard.receiveAttack([6, 6]); //-------
+    gameboard.receiveAttack([7, 6]); //      | Destroyer
+    gameboard.receiveAttack([8, 6]); //-------
+
+    gameboard.receiveAttack([5, 1]); //-------
+    gameboard.receiveAttack([6, 1]); //      | Submarine
+    gameboard.receiveAttack([7, 1]); //-------
+
+    gameboard.receiveAttack([7, 8]); //------- Patrol Boat
+    gameboard.receiveAttack([8, 8]); //-------
+
+    expect(gameboard.checkAllShipsSunk()).toBeTruthy();
+  });
+});
