0d66cb621ce124c6e29bb13c8aebc35cb44e97aa
Erdinch <erdinchorhan02@gmail.com>
2022-09-12 16:00:34 +0300

    player class

--- src/player.js
+++ src/player.js
@@ -1,22 +1,33 @@
 import Gameboard from './gameboard.js';
 
 class Player {
-  constructor(name) {
+  constructor() {
     this.playerBoard = new Gameboard();
     this.opponent = null;
-    this.name = name;
+    this.allShots = [];
   }
 
-  getTurn() {
-    return this.opponent.getPlayerName();
-  }
+  // getTurn() {
+  //   return this.opponent;
+  // }
 
   attacks([x, y]) {
     this.opponent.playerBoard.receiveAttack([x, y]);
+    this.allShots.push([x, y]);
   }
 
-  getPlayerName() {
-    return this.name;
+  checkAlreadyShot([x, y]) {
+    const checkAlreadyShot = this.allShots.some((shot) => {
+      if (shot[0] === x && shot[1] === y) {
+        return true;
+      }
+      return false;
+    });
+
+    if (checkAlreadyShot === true) {
+      return true;
+    }
+    return false;
   }
 
   AIAttacks() {
@@ -25,48 +36,16 @@ class Player {
     };
 
     const attackCoor = [randomNum(), randomNum()];
+    if (this.checkAlreadyShot([attackCoor[0], attackCoor[1]])) {
+      console.log('position is already shot at, retrying');
+      this.AIAttacks();
+    }
+
     this.attacks(attackCoor);
+
+    // return the rando coordinates ?
     console.log(attackCoor);
   }
 }
-
-const PlayerOne = new Player('Human');
-const PlayerAI = new Player('AI');
-PlayerOne.opponent = PlayerAI;
-PlayerAI.opponent = PlayerOne;
-
-console.log(PlayerOne.playerBoard);
-console.log('^----- player one board -----^');
-PlayerOne.playerBoard.placeShip('Carrier', [2, 2], 'hor');
-
-PlayerOne.playerBoard.gameboard.forEach((element) => {
-  console.log(element);
-});
-console.log('^----- player one board game -----^');
-
-console.log(PlayerAI.playerBoard);
-console.log('^----- player ai board -----^');
-PlayerAI.playerBoard.placeShip('Submarine', [4, 1], 'ver');
-
-PlayerAI.playerBoard.gameboard.forEach((element) => {
-  console.log(element);
-});
-console.log('^----- player ai board game -----^');
-
-// player one attacks player ai
-PlayerOne.attacks([4, 1]);
-
-PlayerAI.playerBoard.gameboard.forEach((element) => {
-  console.log(element);
-});
-console.log('^----- player ai board game -----^');
-
-// player ai attacks player one
-PlayerAI.AIAttacks();
-
-PlayerOne.playerBoard.gameboard.forEach((element) => {
-  console.log(element);
-});
-console.log('^----- player one board game -----^');
 
 export default Player;
