aa5e6bba52dc9c89ee78ec34c2199c57cfcac475
Erdinch <erdinchorhan02@gmail.com>
2022-09-13 16:53:47 +0300

    placing some ships to the dispaly grid

--- src/app.js
+++ src/app.js
@@ -8,10 +8,17 @@ const NewPlayer = Player;
 const playerOne = new NewPlayer('Human');
 const playerAI = new NewPlayer('Computer');
 
-// const playerOneGameboard = playerOne.playerBoard;
-// const playerAIGameboard = playerAI.playerBoard;
-
 createPlayerGrid(playerOne);
 createPlayerGrid(playerAI);
 
-playerPlaceShip(playerOne, 'Battleship', [3, 5], 'ver');
+playerPlaceShip(playerOne, 'Carrier', [0, 0], 'hor');
+playerPlaceShip(playerOne, 'Battleship', [1, 7], 'ver');
+playerPlaceShip(playerOne, 'Destroyer', [5, 4], 'ver');
+playerPlaceShip(playerOne, 'Submarine', [3, 1], 'ver');
+playerPlaceShip(playerOne, 'Patrol Boat', [4, 9], 'ver');
+
+playerPlaceShip(playerAI, 'Carrier', [3, 1], 'ver');
+playerPlaceShip(playerAI, 'Battleship', [9, 2], 'hor');
+playerPlaceShip(playerAI, 'Destroyer', [4, 4], 'hor');
+playerPlaceShip(playerAI, 'Submarine', [7, 7], 'hor');
+playerPlaceShip(playerAI, 'Patrol Boat', [0, 2], 'hor');
