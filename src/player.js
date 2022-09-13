17d9341ee8280fb9248643dc56a7af4ec8e92558
Erdinch <erdinchorhan02@gmail.com>
2022-09-13 19:31:32 +0300

    ai can place ships at rando coords

--- src/player.js
+++ src/player.js
@@ -35,12 +35,44 @@ class Player {
     return false;
   }
 
-  AIAttacks() {
-    const randomNum = () => {
-      return Math.floor(Math.random() * 10);
-    };
+  randomNum() {
+    return Math.floor(Math.random() * 10);
+  }
 
-    const attackCoor = [randomNum(), randomNum()];
+  AIPlaceShips() {
+    this.playerBoard.placeShip(
+      'Carrier',
+      [this.randomNum(), this.randomNum()],
+      'ver'
+    );
+    this.playerBoard.placeShip(
+      'Battleship',
+      [this.randomNum(), this.randomNum()],
+      'hor'
+    );
+    this.playerBoard.placeShip(
+      'Destroyer',
+      [this.randomNum(), this.randomNum()],
+      'hor'
+    );
+    this.playerBoard.placeShip(
+      'Submarine',
+      [this.randomNum(), this.randomNum()],
+      'hor'
+    );
+    this.playerBoard.placeShip(
+      'Patrol Boat',
+      [this.randomNum(), this.randomNum()],
+      'ver'
+    );
+
+    if (this.playerBoard.ships.length < 5) {
+      this.AIPlaceShips();
+    } // brute force way of doing things, maybe check if ship exists and only if it doesn't then try that ship with rano coords ?
+  }
+
+  AIAttacks() {
+    const attackCoor = [this.randomNum(), this.randomNum()];
     if (this.checkAlreadyShot([attackCoor[0], attackCoor[1]])) {
       console.log('position is already shot at, retrying');
       this.AIAttacks();
