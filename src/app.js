21bab59caf2fd774c7cf17790f7c447b4a3fc85a
Erdinch <erdinchorhan02@gmail.com>
2022-09-13 19:30:27 +0300

    ai places ships at random coords

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import './css/style.css';
 import './index.html';
 import Player from './player.js';
-import { createPlayerGrid, playerPlaceShip } from './dom-manipulation.js';
+import {
+  createPlayerGrid,
+  playerPlaceShip,
+  AIUpdateDisplay,
+} from './dom-manipulation.js';
 
 const NewPlayer = Player;
 
 const playerOne = new NewPlayer('Human');
 const playerAI = new NewPlayer('Computer');
+playerOne.opponent = playerAI;
+playerAI.opponent = playerOne;
 
 createPlayerGrid(playerOne);
 createPlayerGrid(playerAI);
@@ -17,8 +23,5 @@ playerPlaceShip(playerOne, 'Destroyer', [5, 4], 'ver');
 playerPlaceShip(playerOne, 'Submarine', [3, 1], 'ver');
 playerPlaceShip(playerOne, 'Patrol Boat', [4, 9], 'ver');
 
-playerPlaceShip(playerAI, 'Carrier', [3, 1], 'ver');
-playerPlaceShip(playerAI, 'Battleship', [9, 2], 'hor');
-playerPlaceShip(playerAI, 'Destroyer', [4, 4], 'hor');
-playerPlaceShip(playerAI, 'Submarine', [7, 7], 'hor');
-playerPlaceShip(playerAI, 'Patrol Boat', [0, 2], 'hor');
+playerAI.AIPlaceShips();
+AIUpdateDisplay(playerAI);
