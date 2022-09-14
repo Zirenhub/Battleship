25fc6a4a895af1cf730a1c3e9c9dcef14d74f646
Erdinch <erdinchorhan02@gmail.com>
2022-09-14 15:05:10 +0300

    attacking ships // hitting and missing

--- src/dom-manipulation.js
+++ src/dom-manipulation.js
@@ -11,30 +11,52 @@ const getPlayerContainer = (thisPlayer) => {
   return currentPlayerContainer;
 };
 
+const attackShip = (displayCell, arrayFormat, thisPlayer) => {
+  displayCell.classList.remove('cell');
+  if (thisPlayer.opponent.attacks(arrayFormat)) {
+    displayCell.classList.add('hit');
+  } // example: thisPlayer = playerTwo, when we click on playerTwo's board playerTwo's opponent attacks.
+  displayCell.classList.add('missed');
+};
+
 const createPlayerGrid = (player) => {
   const thisPlayer = player;
   const arrayGrid = thisPlayer.playerBoard.gameboard;
   const playerContainer = getPlayerContainer(thisPlayer);
 
-  arrayGrid.forEach((row) => {
-    row.forEach((cell) => {
-      const displayCell = document.createElement('div');
-      displayCell.classList.add('cell');
-      displayCell.dataset.coor = cell;
-      displayCell.addEventListener('click', () => {
-        // thisPlayer.playerBoard.receiveAttack()
-        // console.log(row);
-        console.log(cell); // find a way to store coordinates in the dataset
+  if (playerContainer === playerTwoContainer) {
+    arrayGrid.forEach((row, rowIndex) => {
+      row.forEach((cell, cellIndex) => {
+        const displayCell = document.createElement('div');
+        displayCell.classList.add('cell');
+        displayCell.dataset.coor = `${rowIndex} ${cellIndex}`;
+        const arrayFormat = [rowIndex, cellIndex];
+        displayCell.addEventListener(
+          'click',
+          () => {
+            attackShip(displayCell, arrayFormat, thisPlayer);
+          },
+          false
+        );
+        playerContainer.appendChild(displayCell);
       });
-      playerContainer.appendChild(displayCell);
-    });
-  });
+    }); // adding eventlistener for playerTwoContainer since this is the container that the human player will be attacking
+  } else {
+    arrayGrid.forEach((row, rowIndex) => {
+      row.forEach((cell, cellIndex) => {
+        const displayCell = document.createElement('div');
+        displayCell.classList.add('cell');
+        displayCell.dataset.coor = `${rowIndex} ${cellIndex}`;
+        playerContainer.appendChild(displayCell);
+      });
+    }); // this is the playerOneContainer and no eventlistener since AI doesn't need to click
+  }
+
   playerContainer.style.gridTemplateColumns = `repeat(${arrayGrid.length}, auto)`;
   playerContainer.style.gridTemplateRows = `repeat(${arrayGrid.length}, auto)`;
 };
 
-const placeShips = (player, playerContainer, ship) => {
-  const arrayGrid = player.playerBoard.gameboard;
+const placeShips = (playerContainer, ship) => {
   const shipPlaced = ship;
   const currentPlayerContainer = playerContainer;
   let shipName = ship.shipClass;
@@ -44,27 +66,16 @@ const placeShips = (player, playerContainer, ship) => {
     shipName = 'patrolBoat';
   } // to be fixed latter ^
   shipPositions.forEach((pos) => {
-    let xPos = pos[0];
-    let yPos = pos[1] + 1; // we add + 1 since grid div's children start from 1 unlike js array's that start from 0
-    if (yPos >= 10) {
-      xPos += 1;
-      yPos = 0;
-    } // if after adding +1 to yPos the value become 10 or greater than 10 then add +1 to xPos and make yPos 0
-    //   for DOM purposes expample: bellow at displayGridPos [4, 9] becomes 50 so we target 50th div of the grid
-    const displayGridPos = Number(`${xPos}${yPos}`); // joint the positions together [3, 5] becomes 36
-    const playerGridPos = currentPlayerContainer.childNodes[displayGridPos]; // get the 36th grid div cell
-    playerGridPos.dataset.coor = arrayGrid[pos[0]][pos[1]]; // replace its dataset with the name of ship being placed `${shipName}`
-    playerGridPos.setAttribute('id', `${shipName}`); // give the div's id with the name of the ship being placed
-    // console.log(arrayGrid[pos[0]][pos[1]]);
+    const xPos = pos[0]; // store the first coordinate exp: [3, 5] store 3
+    const yPos = pos[1]; // store the second coordinate
+    const dataID = `${xPos} ${yPos}`; // store both of the coordinates into a string exp: 3 5
+
+    const child = currentPlayerContainer.querySelector(
+      `[data-coor='${dataID}']`
+    ); // ^ store the child div of the currentPlayerContainer with the current dataID
+    child.setAttribute('id', `${shipName}`); // set the div's id with the name of the ship being placed
   });
-
   // most likely not the best way of doing this, but the only solution i can think of 😕
-
-  // arrayGrid.forEach((row) => {
-  //   row.forEach((cell) => {
-  //     console.log(cell);
-  //   });
-  // });
 };
 
 const playerPlaceShip = (player, shipClass, [x, y], dir) => {
@@ -81,14 +92,14 @@ const playerPlaceShip = (player, shipClass, [x, y], dir) => {
   } // place the ship in players gameboard // if false is returned then exit, something went wrong
 
   const ship = playerGameboard.getShip(shipPlaced); // store the ship
-
-  placeShips(player, playerOneContainer, ship);
+  placeShips(playerOneContainer, ship);
 };
 
 const AIUpdateDisplay = (playerAI) => {
   const AIShips = playerAI.playerBoard.ships;
+
   AIShips.forEach((ship) => {
-    placeShips(playerAI, playerTwoContainer, ship);
+    placeShips(playerTwoContainer, ship);
   });
 };
 
